6aa8124a0c5c6109a1c2eeda04c860b779f12153
Ravi Reddy <arkr87@gmail.com>
2025-01-06 12:38:47 -0600

    expense shellscript

---
+++ mysql.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+VALIDATE() {
+            if [ $1 -ne 0 ]
+            then 
+              echo "$2....Failure"
+            else 
+              echo "$2....Success"
+            fi
+}
+
+
+CHECK_ROOT() {
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:Please make sure that you are a root user"
+    exit 1
+fi
+}
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySql SERVER"
+
+systemctl enable mysqld >>$LOG_FILE_NAME
+VALIDATE $? "Enable  MySql SERVER"
+
+systemctl start mysqld >>$LOG_FILE_NAME
+VALIDATE $? "starting  MySql SERVER"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "setting up root password"
+
+
+
+
+
+
+
+#
+#for package in $@
+#do 
+ #   dnf list installed $package &>>$LOG_FILE_NAME
+ #   if [ $? -ne 0 ] 
+  #  then 
+  #        dnf install $package -y &>>$LOG_FILE_NAME
+  #        VALIDATE $? "Installing $package"
+  #  else 
+  #        echo "$package already INSTALLED"
+  #  fi
+
+#done
