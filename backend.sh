b15f6ea9647592f72a7a115cce8b72c48501fe3a
Ravi Reddy <arkr87@gmail.com>
2025-01-07 15:32:42 -0600

    pushing changes made to backend ss

--- backend.sh
+++ backend.sh
@@ -38,7 +38,7 @@ dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "installing nodejs SERVER"
 
 id expense &>>$LOG_FILE_NAME
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then 
     useradd expense &>>$LOG_FILE_NAME
     VALIDATE $? "Adding user to expense"
